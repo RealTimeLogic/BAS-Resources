aadaf6896454b29ffcd7d422d52590012b486ba1
RTL <24235755+Telemechanics@users.noreply.github.com>
2026-08-28 08:46:44 +0200

    New function ba.tpm.createcertificate

--- src/misc/MakoTPM.lua
+++ src/misc/MakoTPM.lua
@@ -1,7 +1,7 @@
 local maxHash=pcall(function() ba.crypto.hash("sha512") end) and "sha512" or "sha256"
-local sfmt,jencode,jdecode,symmetric,PBKDF2,keyparams,sign,jwtsign,createkey,createcsr,sharkcert=
+local sfmt,jencode,jdecode,symmetric,PBKDF2,keyparams,sign,jwtsign,createkey,createcsr,createcertificate,sharkcert=
 string.format,ba.json.encode,ba.json.decode,ba.crypto.symmetric,ba.crypto.PBKDF2,ba.crypto.keyparams,
-ba.crypto.sign,require"jwt".sign,ba.create.key,ba.create.csr,ba.create.sharkcert
+ba.crypto.sign,require"jwt".sign,ba.create.key,ba.create.csr,ba.create.certificate,ba.create.sharkcert
 local function setuser(ju,db,name,pwd)
    if pwd then
       if type(pwd) == "string" then
@@ -25,6 +25,13 @@ local function tpm(gpkey,upkey)
    local function tpmJwtsign(p,kname,op) return jwtsign(p,function(h) return sign(h,tpmGetKey(kname)) end,op) end
    local function tpmKeyparams(kname) return keyparams(tpmGetKey(kname)) end
    local function tpmCreatecsr(kname,...) return createcsr(tpmGetKey(kname),...) end
+   local function tpmCreatecertificate(kname,csr,certOrValidFrom,validFromOrValidTo,validToOrSerial,serialOrHash,hash)
+      local key=tpmGetKey(kname)
+      if type(certOrValidFrom) == "string" then
+	 return createcertificate(csr,certOrValidFrom,key,validFromOrValidTo,validToOrSerial,serialOrHash,hash)
+      end
+      return createcertificate(csr,key,certOrValidFrom,validFromOrValidTo,validToOrSerial,serialOrHash)
+   end
    local function tpmCreatekey(kname,op)
       if keys[kname] then error(sfmt("ECC key %s exists",kname),2) end
       op = op or {}
@@ -43,6 +50,7 @@ local function tpm(gpkey,upkey)
    function t.haskey(k) return tpmHaskey(k) end
    function t.createkey(k,...) return tpmCreatekey(k,...) end
    function t.createcsr(k,...) return tpmCreatecsr(k,...) end
+   function t.createcertificate(k,...) return tpmCreatecertificate(k,...) end
    function t.sign(h,k,o) return tpmSign(h,k,o) end
    function t.jwtsign(k,...) return tpmJwtsign(k,...) end
    function t.keyparams(k,...) return tpmKeyparams(k,...) end
